d20089255ec815e0bcaac11351db738f692fcf72
fourre.louis@gmail.com <fourrE.louis@gmail.com>
2025-07-08 15:44:21 +0800

    ADD: New version with Ultralytics Solutions and a line counter to count the melons

--- detect.py
+++ detect.py
@@ -1,30 +1,49 @@
-from ultralytics import YOLO
-import sys, time, torch
-
-# Parameters
-video_path = sys.argv[1] if len(sys.argv) > 1 else "No video path provided."
-
-# Model parameters
-conf_threshold = 0.7
-
-model = YOLO("models/detect/V3/weights/best.pt")
-model.to('cuda' if torch.cuda.is_available() else 'cpu') # Need pytorch with cuda support!!
+import sys, time, torch, cv2
+from ultralytics import solutions
 
 t_start = time.perf_counter()
 
-results = model.track(video_path, show=True, conf=conf_threshold, save=True, stream=True,tracker="tracker.yaml",persist=True)
+# Model parameters
+conf_threshold = 0.8
 
-# get the last seen melon ids, cause each id is unique, last id is also the nulber of melons
-last_seen_id = -1
+# Video parameters
+video_path = sys.argv[1] if len(sys.argv) > 1 else "No video path provided."
+cap = cv2.VideoCapture(video_path)
+assert cap.isOpened(), "Error reading video file"
 
-for result in results:
-    if result.boxes is not None and result.boxes.id is not None:
-        ids = result.boxes.id.cpu().numpy().astype(int)
-        max_id_in_frame = ids.max(initial=-1)
-        last_seen_id = max(last_seen_id, max_id_in_frame)
-        
+# Video writer
+w, h, fps = (int(cap.get(x)) for x in (cv2.CAP_PROP_FRAME_WIDTH, cv2.CAP_PROP_FRAME_HEIGHT, cv2.CAP_PROP_FPS))
+video_writer = cv2.VideoWriter("object_counting_output.avi", cv2.VideoWriter_fourcc(*"mp4v"), fps, (w, h))
+
+region_points = [(int(w/2), 0), (int(w/2), (int(h)))] # line counting
+
+
+# Initialize object counter object
+counter = solutions.ObjectCounter(
+    show=True,  # display the output
+    region=region_points,  # pass region points
+    model="models/detect/V2/weights/best.pt", 
+    conf=conf_threshold,  # confidence threshold
+    show_out=False,
+    tracker="trackerV2.yaml",
+    device='cuda' if torch.cuda.is_available() else 'cpu',
+)
+
+# Process video
+while cap.isOpened():
+    success, im0 = cap.read()
+
+    if not success:
+        print("Video frame is empty or processing is complete.")
+        break
+
+    results = counter(im0)
+    video_writer.write(results.plot_im)  # write the processed frame.
+
 t_stop = time.perf_counter()
 
-# Print results
-print("Number of melon: ", last_seen_id)
 print("Time taken to compute: ",t_stop - t_start)
+print("Number of Melons: ", results.in_count)
+cap.release()
+video_writer.release()
+cv2.destroyAllWindows()  # destroy all opened windows
